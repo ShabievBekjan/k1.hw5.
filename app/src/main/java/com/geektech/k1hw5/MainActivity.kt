7c6c6981df1c8b97f84f6434db240012918a9dab
ShabievBekjan <beka.shabiev@gmail.com>
2022-10-10 18:56:18 +0600

    k

--- app/src/main/java/com/geektech/k1hw5/MainActivity.kt
+++ app/src/main/java/com/geektech/k1hw5/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.geektech.k1hw5
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.geektech.k1hw5.databinding.ActivityMainBinding
 
-        class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
             private var count = 0
             private lateinit var result: String
             private lateinit var binding: ActivityMainBinding
@@ -17,12 +19,12 @@ import android.os.Bundle
             fun onClick() {
                 binding.btnOne.setOnClickListener {
                     count += 1
-                    binding.tvOne.text = count.toString()
+                    binding.TvOne.text = count.toString()
                     if (count == 10 ) {
                         binding.btnOne.text = "-1"
                         binding.btnOne.setOnClickListener {
                             count -= 1
-                            binding.tvOne.text = count.toString()
+                            binding.TvOne.text = count.toString()
                             if (count == 0) {
                                 val intent = Intent(this, SecondActivity::class.java)
                                 startActivity(intent)
@@ -32,5 +34,4 @@ import android.os.Bundle
                 }
             }
         }
-    }
-}
+
---
+++ app/src/main/java/com/geektech/k1hw5/SecondActivity.kt
@@ -0,0 +1,11 @@
+package com.geektech.k1hw5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
